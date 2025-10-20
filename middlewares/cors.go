67128adaa220c958f3324a6103484ae2e2c0a5a4
unknown <kritsada.kjt@gmail.com>
2025-10-21 06:59:11 +0700

    change url origin

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,13 +1,16 @@
 package middlewares
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func CorsConfig() func(*fiber.Ctx) error {
+	frontendURL := os.Getenv("FRONTEND_URL")
 	return cors.New(cors.Config{
-		AllowOrigins:     "https://pig-farm-frontend.vercel.app,http://localhost:3000",
+		AllowOrigins:     "http://localhost:3000," + frontendURL,
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS,PATCH",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
